6e53174af0167ebea3b1690566a9c7b34420f6eb
sagunji <karanjit.sagun01@gmail.com>
2020-04-19 16:01:07 +0545

    Add support for netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
 const withImages = require('next-images');
-module.exports = withImages({}); 
+
+module.exports = withImages({
+  // Target must be serverless
+  target: 'serverless',
+  // distDir already has .next as default
+  distDir: '.next'
+});
