a6cc3802a3b1f66b7315f47d18fba084d1dcd142
sagunji <karanjit.sagun01@gmail.com>
2020-04-19 16:11:20 +0545

    Remove serverless as target

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const withImages = require('next-images');
 
 module.exports = withImages({
-  // Target must be serverless
-  target: 'serverless',
   // distDir already has .next as default
   distDir: '.next'
 });
